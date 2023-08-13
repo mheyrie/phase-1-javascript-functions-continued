c0c69f246d126d459b1bea5ba43878891634b011
Mary <omota4fidelity@gmail.com>
2023-08-13 20:02:15 +0100

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(sign){
+    if (sign === "*"){
+    return function (msg="special"){
+        return `You are ${sign}${msg}${sign}!`;
+    }
+}
+else if (sign === "||"){
+    return function (msg = "special"){
+        return `You are ${sign}${msg}${sign}!`;
+    }
+}
+ 
+}
+
+
+
